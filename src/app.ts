2d0ab95afb037a8374e81f08f7a488489f953a08
shaharsol <shahar@gnift.com>
2024-03-05 11:22:26 +0200

    showing dashboard

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import usersRouter from './routers/users';
+import path from 'path';
 
 const server = express();
+server.set('views', path.resolve(__dirname, 'views'));
+server.set('view engine', 'ejs');
 
 server.use('/users', usersRouter);
 
