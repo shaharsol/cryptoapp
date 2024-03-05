4021bf2f586b528055c6b6d42837c36a651b3bf6
shaharsol <shahar@gnift.com>
2024-03-05 16:29:36 +0200

    error handler

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,20 @@ import express from 'express';
 import usersRouter from './routers/users';
 import path from 'path';
 import config from 'config';
+import errorHandler from './middlewares/error/error-handler';
 
 const server = express();
 server.set('views', path.resolve(__dirname, 'views'));
 server.set('view engine', 'ejs');
 
+// general middlewares
 server.use(express.urlencoded())
+
+// routing
 server.use('/users', usersRouter);
+
+// error middlewares
+server.use(errorHandler);
 
 const port = config.get<number>('app.port');
 server.listen(port, () => {
---
+++ src/middlewares/error/error-handler.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+    res.render('error', {
+        err
+    })
+}
