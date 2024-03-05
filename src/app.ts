319636c535a0141e517bbdff425ab1dcab2f0d80
shaharsol <shahar@gnift.com>
2024-03-05 14:19:57 +0200

    added config to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import usersRouter from './routers/users';
 import path from 'path';
+import config from 'config';
 
 const server = express();
 server.set('views', path.resolve(__dirname, 'views'));
@@ -8,6 +9,7 @@ server.set('view engine', 'ejs');
 
 server.use('/users', usersRouter);
 
-server.listen(8080, () => {
-    console.log('started jeep...');
+const port = config.get<number>('app.port');
+server.listen(port, () => {
+    console.log(`Server listening on ${port}`);
 })
