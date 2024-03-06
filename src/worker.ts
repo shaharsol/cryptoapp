d991b0a4da87e5c8ea63a21faca773d670f75891
shaharsol <shahar@gnift.com>
2024-03-06 10:24:15 +0200

    initial worker

---
+++ src/worker.ts
@@ -0,0 +1,41 @@
+import mysql from 'mysql2';
+import util from 'util';
+import config from 'config';
+import mongoose from 'mongoose';
+
+// mysql init
+const connection = mysql.createConnection(config.get('mysql'));
+const connect = util.promisify(connection.connect).bind(connection);
+const query = util.promisify(connection.query).bind(connection);
+
+// mongo init
+const host = config.get<string>('mongo.host');
+const port = config.get<number>('mongo.port');
+const database = config.get<string>('mongo.database');
+
+// function scrape
+// fetch data from google
+// save in mongo
+// notify clients
+
+// loop
+// get symbols from mysql
+// scrape all symbols
+// set timeout for next cycle
+async function work() {
+    try {
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        setTimeout(work, config.get<number>('worker.interval'))
+    }
+}
+
+(async () => {
+    await Promise.all([
+        connect(),
+        mongoose.connect(`mongodb://${host}:${port}/${database}`)
+    ])
+    work();
+})
